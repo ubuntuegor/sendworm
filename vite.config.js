245f5ef84ab68097d4103d7762f358526b747524
Egor Kukoverov <me@bnt.to>
2025-05-02 00:48:22 +0300

    setup formatter, add basic page with file selection capabilities

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite"
+import { sveltekit } from "@sveltejs/kit/vite"
 
 // @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -29,4 +29,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+}))
